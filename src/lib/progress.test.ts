71c4c410647ac1ced33edad3890ffca2d106cc1d
isqua <isqua@isqua.ru>
2025-01-10 22:17:54 +0100

    feat: introduce month progress calculator

---
+++ src/lib/progress.test.ts
@@ -0,0 +1,57 @@
+import { describe, expect, it } from 'vitest';
+import { calculateMonthsProgress, type ProgressItem } from './progress';
+
+describe('calculateMonthsProgress', () => {
+    it('should return empty array if start date is after end date', () => {
+        const startDate = new Date('2001-01-01');
+        const endDate = new Date('2000-01-01');
+        const today = new Date('1970-01-01');
+        const progress = calculateMonthsProgress(startDate, endDate, today);
+
+        expect(progress).toHaveLength(0);
+    });
+
+    it('should return one month if start and end date are in the same month', () => {
+        const startDate = new Date('2001-01-01');
+        const endDate = new Date('2001-01-31');
+        const today = new Date('1970-01-01');
+        const progress = calculateMonthsProgress(startDate, endDate, today);
+
+        expect(progress).toEqual<ProgressItem[]>([
+            {
+                year: 2001,
+                month: 0,
+                value: 0,
+                max: 31,
+            },
+        ]);
+    });
+
+    it('should return full months if start and end date are in the middle of months', () => {
+        const startDate = new Date('2001-01-10');
+        const endDate = new Date('2001-03-19');
+        const today = new Date('2001-02-12');
+        const progress = calculateMonthsProgress(startDate, endDate, today);
+
+        expect(progress).toEqual<ProgressItem[]>([
+            {
+                year: 2001,
+                month: 0,
+                value: 31,
+                max: 31,
+            },
+            {
+                year: 2001,
+                month: 1,
+                value: 12,
+                max: 28,
+            },
+            {
+                year: 2001,
+                month: 2,
+                value: 0,
+                max: 31,
+            },
+        ]);
+    });
+});
---
+++ src/lib/progress.ts
@@ -0,0 +1,50 @@
+export type ProgressItem = {
+    year: number;
+    month: number;
+    max: number;
+    value: number;
+};
+
+export function calculateMonthsProgress(
+    start: Date,
+    end: Date,
+    today: Date
+): ProgressItem[] {
+    const result: ProgressItem[] = [];
+
+    let currentDate = new Date(start);
+    currentDate.setDate(1);
+
+    while (currentDate <= end) {
+        const year = currentDate.getFullYear();
+        const month = currentDate.getMonth();
+        const daysInMonth = new Date(year, month + 1, 0).getDate();
+
+        let value = 0;
+
+        if (
+            currentDate.getFullYear() < today.getFullYear() ||
+            currentDate.getMonth() < today.getMonth()
+        ) {
+            value = daysInMonth;
+        }
+
+        if (
+            currentDate.getFullYear() === today.getFullYear() &&
+            currentDate.getMonth() === today.getMonth()
+        ) {
+            value = today.getDate();
+        }
+
+        result.push({
+            year,
+            month,
+            max: daysInMonth,
+            value,
+        });
+
+        currentDate.setMonth(currentDate.getMonth() + 1);
+    }
+
+    return result;
+}
