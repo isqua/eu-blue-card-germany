3902bf2fe683d01a204f17ed9df383578d59b73e
isqua <isqua@isqua.ru>
2025-01-10 21:01:33 +0100

    feat: introduce ProgressBar component

--- src/App.svelte
+++ src/App.svelte
@@ -1,2 +1,7 @@
+<script lang="ts">
+    import ProgressBar from './components/ProgressBar.svelte';
+</script>
+
 <main>
+    <ProgressBar value={5} max={31}>Hello world</ProgressBar>
 </main>
---
+++ src/components/ProgressBar.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+    import type { Snippet } from 'svelte';
+
+    interface Props {
+        value?: number;
+        max?: number;
+        children?: Snippet;
+    }
+
+    const { value = 0, max = 30, children }: Props = $props();
+    const percentage = Math.round((value / max) * 100);
+</script>
+
+<div class="progress-container">
+    <div class="progress-bar" style="width: {percentage}%"></div>
+    <span class="progress-label">
+        {@render children?.()}
+    </span>
+</div>
+
+<style>
+    .progress-container {
+        width: 100%;
+        background-color: var(--muted-background-color);
+        border-radius: 4px;
+        overflow: hidden;
+        position: relative;
+    }
+
+    .progress-bar {
+        background-color: #0ab40a;
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        z-index: 0;
+    }
+
+    .progress-label {
+        color: var(--text-color);
+        font-weight: bold;
+        padding: 0 5px;
+        line-height: 20px;
+        z-index: 1;
+        position: relative;
+    }
+</style>
